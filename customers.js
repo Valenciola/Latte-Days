0e57b533a030f23d2d272ac1a7698bc87b1b69c8
Valenciola <valeriez3012@gmail.com>
2025-04-23 23:47:08 -0400

    Additional plans + comments for clarity

--- customers.js
+++ customers.js
@@ -1,7 +1,27 @@
-import { Customer, Order } from "./structures.js";
+import { Customer, Order, Interaction } from "./structures.js";
+
+/*
+class Customer { // Add first visiting day, a special order, friendship level buffer (how fast you can be friends)
+    constructor(name = "", order = Order, orderdesc = "", placed = "", chat, recieve = "", judgings = []) {
+        this.name = name || "?";
+        this.order = order || null;
+        this.orderdesc = orderdesc || "?";
+        this.placed = placed || "Thank you.";
+        this.chat = chat || true; // Flag for if they're chattable or not, reset upon day assignment
+        this.recieve = recieve || "That's for me.";
+        this.judgings = judgings || [ // Perfect, Good, Okay, BAD
+            "This is perfect! Thank you!", 
+            "Thank you very much.", 
+            "This looks okay... thanks.", 
+            "I didn't order this."
+        ];
+    }
+}
+*/
 
 // Characters
 let Chima = new Customer(
+    // Calm, kind, friendly
     "Chima", 
     new Order("Espresso", "Mocha", [], ["Cold Foam", "Chocolate Sauce"]), 
     "I'd like a mocha espresso topped with cold foam and chocolate sauce, please!",
@@ -12,11 +32,12 @@ let Chima = new Customer(
         "This is perfect! Thank you!", 
         "Thank you very much.", 
         "This looks okay... thanks.", 
-        "I didn't order this."
+        "I didn't order this..."
     ]
 );
 
 let Shon = new Customer(
+    // Hyper, extroverted, opposite of Devon
     "Shon",
     new Order("Americano", "Chai", ["Ice"]), 
     "I'd like an iced chai Americano, please!",
@@ -24,14 +45,15 @@ let Shon = new Customer(
     true, 
     "Oh, my drink is ready!",
     [
-        "This is perfect! Thank you!", 
-        "Thank you very much.", 
-        "This looks okay... thanks.", 
-        "I didn't order this."
+        "Yes! I've been waiting for this! Thank you so much!", 
+        "This looks good! Can't wait to enjoy it. Thanks!", 
+        "This seems a bit weird, but I'll take it anyway.", 
+        "So... I don't recall making this order, actually..."
     ]
 );
 
 let Devon = new Customer(
+    // Calm, introverted, opposite of Shon
     "Devon",
     new Order("Tea", "Matcha", ["Milk"]), 
     "I'll have a matcha tea with milk, please",
@@ -39,10 +61,10 @@ let Devon = new Customer(
     true, 
     "I'm here.",
     [
-        "This is perfect! Thank you!", 
-        "Thank you very much.", 
-        "This looks okay... thanks.", 
-        "I didn't order this."
+        "Just as I expected. I appreciate it.", 
+        "Thank you. Have a nice day.", 
+        "...This is a bit different than expected...", 
+        "I didn't order this. Are you sure you heard me correctly?"
     ]
 );
 
--- structures.js
+++ structures.js
@@ -27,13 +27,14 @@ class Order {
     }
 }
 
-class Customer { // Add first visiting day, a special order, friendship level buffer (how fast you can be friends)
+class Customer { 
+    // Add first visiting day, a special order, friendship level buffer (how fast you can be friends), friendship level, interactions, times visited
     constructor(name = "", order = Order, orderdesc = "", placed = "", chat, recieve = "", judgings = []) {
         this.name = name || "?";
         this.order = order || null;
         this.orderdesc = orderdesc || "?";
-        this.placed = placed || "Thank you.";
-        this.chat = chat || true;
+        this.placed = placed || "Thank you."; // What they say after you submit the order
+        this.chat = chat || true; // Flag for if they're chattable or not, reset upon day assignment
         this.recieve = recieve || "That's for me.";
         this.judgings = judgings || [ // Perfect, Good, Okay, BAD
             "This is perfect! Thank you!", 
@@ -44,4 +45,13 @@ class Customer { // Add first visiting day, a special order, friendship level bu
     }
 }
 
-export {Ticket, Drink, Order, Customer};
+class Interaction { // Each character should keep an array of these (to be sifted at the chat button)
+    constructor(conditional = Boolean, dialogue = []) {
+        this.conditional = conditional || false;
+        this.dialogue = dialogue || [
+            ["Name", "Empty"]
+        ];
+    }
+}
+
+export {Ticket, Drink, Order, Customer, Interaction};
